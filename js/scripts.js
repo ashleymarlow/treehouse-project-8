c3cb65dc747dadf920bdd100dcc5aa03136fed21
Ashley Marlow <ashley.marlow@hotmail.com>
2022-08-24 20:12:58 -0700

    starting on the modal!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const employeeContainer = document.getElementById('employee-container');
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(response => response.json())
     .then(data => generateCards(data.results));
 
@@ -7,10 +7,30 @@ function generateCards(data) {
     const cards = data.map(item => `
     <div class='card'>
         <img src="${item.picture.medium}">
-        <span class="name">${item.name.first} ${item.name.last}</span>
-        <span class="email">${item.email}</span>
-        <span class="city">${item.location.city}</span>
+        <div class="employee-info" 
+            data-phone-number="${item.phone}" 
+            data-address="${item.location.street.number} ${item.location.street.name} ${item.location.city}, ${item.location.state} ${item.location.postcode}" 
+            data-birthday="${item.dob.date.slice(0,10)}">
+                <span class="name">${item.name.first} ${item.name.last}</span>
+                <span class="email">${item.email}</span>
+                <span class="city">${item.location.city}</span>
+        </div>
     </div>
     `).join('');
     employeeContainer.innerHTML = cards;
+}
+
+let modalBtn = document.getElementById("modal-btn");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
+modalBtn.onclick = function(){
+  modal.style.display = "block";
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none";
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none";
+  }
 }
