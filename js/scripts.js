be59a5cbe14ef6e4f48fae45c85f2ecf7f5ead16
Ashley Marlow <ashley.marlow@hotmail.com>
2022-08-25 21:38:53 -0700

    finished!!

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
 function generateCards(data) {
     const cards = data.map(item => `
     <div class='card'>
-        <img src="${item.picture.medium}">
+        <img src="${item.picture.large}">
         <div class="employee-info" 
             data-phone-number="${item.phone}" 
             data-address="${item.location.street.number} ${item.location.street.name} ${item.location.city}, ${item.location.state} ${item.location.postcode}" 
@@ -27,8 +27,8 @@ function generateModal() {
     let closeBtn = document.querySelector(".close-btn");
 
     for (let i = 0; i < cards.length; i++) {
-        cards[i].onclick = function(){
-            populateModal();
+        cards[i].onclick = function(e){
+            populateModal(e);
             modal.style.display = "block";
         }
     }
@@ -44,8 +44,32 @@ function generateModal() {
     }
 }
 
-function populateModal() {
-    console.log('populate modal');
+function populateModal(e) {
+    //step 1 - Get Modal HTML
+    const modalImage = document.querySelector('.modal-image');
+    const modalName = document.querySelector('.modal-name');
+    const modalEmail = document.querySelector('.modal-email');
+    const modalCity = document.querySelector('.modal-city');
+    const modalPhone = document.querySelector('.modal-phone');
+    const modalAddr = document.querySelector('.modal-addr');
+    const modalDate = document.querySelector('.modal-date');
+
+    //step 2 - Get Clicked On Data 
+    const card = e.target.closest('.card');
+    const cardImage = card.children[0].src;
+    const cardName = card.children[1].children[0].innerHTML;
+    const cardEmail = card.children[1].children[1].innerHTML;
+    const cardCity = card.children[1].children[2].innerHTML;
+    const cardPhone = card.children[1].dataset.phoneNumber;
+    const cardAddr = card.children[1].dataset.address;
+    const cardDate = card.children[1].dataset.birthday;
+
+    //step 3 - Populate Modal With Data
+    modalImage.src = cardImage;
+    modalName.innerHTML = cardName;
+    modalEmail.innerHTML = cardEmail;
+    modalCity.innerHTML = cardCity;
+    modalPhone.innerHTML = cardPhone;
+    modalAddr.innerHTML = cardAddr;
+    modalDate.innerHTML = "Birthday: "+ cardDate;
 }
-
-
